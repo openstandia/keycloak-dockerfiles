d801c96cd5f08355b55408db5e0afc1180d3055b
svu00012 <svu00012@partner.nri.co.jp>
2019-02-21 12:21:30 +0900

    updated wait-for-kc.sh

--- client_adapter-based-arch-examples/kc-tomcat-adapter-example/tomcat2/wait-for-kc.sh
+++ client_adapter-based-arch-examples/kc-tomcat-adapter-example/tomcat2/wait-for-kc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAX_LOOP=24
+MAX_LOOP=30
 CNT_LOOP=1
 
 while [ $CNT_LOOP -lt $MAX_LOOP ]; do
@@ -11,15 +11,15 @@ while [ $CNT_LOOP -lt $MAX_LOOP ]; do
     if [ $? -ne 0 ]; then
         CNT_LOOP=$((CNT_LOOP+1))
     else
-        break
+        echo "keycloak ssl connection ok!"
+        /usr/local/tomcat/bin/catalina.sh run
     fi
     echo "waiting 10 sec ... "
     sleep 10
 
 done
 
-echo "keycloak ssl connection ok!"
-/usr/local/tomcat/bin/catalina.sh run
-
+echo "keycloak ssl connection check error"
+echo "Tomcat couldn't start"
 
 
